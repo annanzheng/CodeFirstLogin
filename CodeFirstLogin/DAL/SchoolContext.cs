1961e61de4ffca7bcefec719f656090131b99acd
annanzheng <annanzh@umich.edu>
2024-07-01 18:45:48 -0400

    Add project files.

---
+++ CodeFirstLogin/DAL/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CodeFirstLogin.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CodeFirstLogin.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ CodeFirstLogin/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CodeFirstLogin.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+        [NotMapped]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        // public HttpPostedFileBase ProfileImage { get; set; }
+
+        // public string ProfileImagePath { get; set; }
+    }
+}
